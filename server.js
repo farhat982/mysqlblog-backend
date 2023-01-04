1ba101759e98eb70d363ba145c7fd9b9da851076
farhat982 <112558257+farhat982@users.noreply.github.com>
2023-01-04 05:01:48 +0000

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cookieParser())
 app.use(cors())
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '../uploads')
+    cb(null, 'https://mysqlblog-frontend.onrender.com/../uploads')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
