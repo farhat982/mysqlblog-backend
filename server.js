e074f44096775f3a3e4aef4726398fd6c5583a4e
farhat982 <farhat.ikram@gmail.com>
2023-01-05 06:31:36 +0000

    new image upload functionnality aded

--- server.js
+++ server.js
@@ -1,40 +1,49 @@
-import express from 'express'
-import authRoutes from './routes/auth.js'
-import userRoutes from './routes/users.js'
-import postRoutes from './routes/posts.js'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import multer from 'multer'
+import express, { urlencoded } from 'express';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import bodyParser from 'body-parser';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'https://mysqlblog-frontend.onrender.com/mysqlblog-frontend/public/uploads/')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-    cb(null, Date.now() + file.originalname)
-  },
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const upload = multer({ storage: storage })
+app.use(bodyParser.json());
 
-//const upload = multer({ dest: './uploads/' })
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: '*',
+	})
+);
 
-app.post('/api/upload', upload.single('file'), function (req, res) {
-  const file = req.file
-  res.status(200).json(file.filename)
-})
+cloudinary.config({
+	cloud_name: 'deg348784',
+	api_key: '726844471184658',
+	api_secret: 'jH4-oJBaTuf1BYDdXnm5PCYpewk',
+});
+const storage = new CloudinaryStorage({
+	cloudinary: cloudinary,
+	params: {
+		folder: 'DEV1',
+	},
+});
 
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/posts', postRoutes)
-app.use('/api/posts', postRoutes)
+const upload = multer({ storage: storage });
+
+app.post('/api/upload', upload.single('file'), async (req, res) => {
+	return res.json(req.file.path);
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 app.listen(8000, () => {
-  console.log('Server is running on port 8000')
-})
+	console.log('Server is running on port 8000');
+});
